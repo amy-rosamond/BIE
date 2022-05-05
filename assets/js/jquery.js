3c52fd4b68010379b387d682977c88110e952eac
amy-rosamond <amyrosamond@gmail.com>
2022-05-05 17:00:05 +0100

    dropdown work started

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -14,3 +14,24 @@ $(function () {
     resize();
     $(window).resize(resize)
 });
+
+
+$(function(){
+    $("div.installation-item-more-info").click(function(){
+        if ($("div.dropdown-content").hasClass("expanded")) {
+            $("div.dropdown-content.expanded").removeClass("expanded").slideUp(250);
+            $(this).removeClass("open");
+        } else {
+            $("div.dropdown-content").addClass("expanded").slideDown(250);
+            $(this).addClass("open");
+        }
+    });
+});
+
+
+// $(document).ready(function(){
+//     $("div.installation-item-more-info").click(function(){
+//       $("div.dropdown-content").slideToggle(1500);
+//       $(this).toggleClass("open");
+//     });
+//   });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,48 @@
-const accordionBtns = document.querySelectorAll(".installation-item-more-info");
+// const accordionBtns = document.querySelectorAll(".installation-list");
+// console.log(accordionBtns)
+// accordionBtns.forEach((accordion) => {
+//   accordion.onclick = function () {
+//     this.classList.toggle("active");
 
-accordionBtns.forEach((accordion) => {
-  accordion.onclick = function () {
-    this.classList.toggle("active");
+//     let content = this.nextElementSibling;
+//     console.log(content);
 
-    let content = this.nextElementSibling;
-    console.log(content);
+//     if (content.style.maxHeight) {
+//       //this is if the accordion is open
+//       content.style.maxHeight = null;
+//     } else {
+//       //if the accordion is currently closed
+//       content.style.maxHeight = content.scrollHeight + "px";
+//       console.log(content.style.maxHeight);
+//     }
+//   };
+// });
 
-    if (content.style.maxHeight) {
-      //this is if the accordion is open
-      content.style.maxHeight = null;
-    } else {
-      //if the accordion is currently closed
-      content.style.maxHeight = content.scrollHeight + "px";
-      console.log(content.style.maxHeight);
-    }
-  };
-});
+
+// $(function() {
+//   $('.installation-list .dropdown-content').hide()
+//   $('.installation-list .installation-item').on('click',function() {
+//       $('.installation-list .dropdown-content').toggle()
+//   })
+// })
+
+
+// const accordionBtns = document.querySelectorAll(".installation-list");
+
+// accordionBtns.forEach((accordion) => {
+//   accordion.onclick = function () {
+//     this.classList.toggle("active");
+
+//     let content = this.nextElementSibling;
+//     console.log(content);
+
+//     if (content.style.maxHeight) {
+//       //this is if the accordion is open
+//       content.style.maxHeight = null;
+//     } else {
+//       //if the accordion is currently closed
+//       content.style.maxHeight = content.scrollHeight + "px";
+//       console.log(content.style.maxHeight);
+//     }
+//   };
+// });
